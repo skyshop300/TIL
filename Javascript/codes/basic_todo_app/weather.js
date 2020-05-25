2a056ff1a3b66069d50ddf3d43dab4720b6a6591
skyshop300 <skyshop5210@gmail.com>
2020-05-25 21:19:42 +0900

    basic-tool-app: 날씨 API 적용 - [프로젝트 안료]

--- Javascript/codes/basic_todo_app/weather.js
+++ Javascript/codes/basic_todo_app/weather.js
@@ -1,8 +1,26 @@
-
 const COORDS = 'coords'; 
+const weather = document.querySelector(".js-weather");
+
+function getWeather(lat, lon) {
+	fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
+	// then:  함수가 끝나길 기다리는 방법
+	.then(function(response) {		
+		// 성공시 실행		
+		return response.json();
+	}).then(function(json) {
+		// 성공시 실행
+		const temperature = json.main.temp;
+		const place = json.name;
+		weather.innerText = `${temperature} @ ${place}`
+
+	});
+	// fetch를 기다리지 않고 다음 작업을 지시하면 
+	// 다음 작업은 fetch가 완료 되기를 기다리지 않고, 원하지 않는 결과가 나올 수 있다.
+	// 서버로 부터 데이터가 들어올때까지 기다려야한다.
+}
 
 // localStorage를 사용하지 않고 DNC
-function saveCords(coordsObj) {
+function saveCoords(coordsObj) {
 	localStorage.setItem(COORDS, JSON.stringify(coordsObj));
 }
 
@@ -15,31 +33,34 @@ function handleGeoSuccess(position) {
 	// };
 	// 변수명이 동일할 때에는 아래와 같이 변수명을 따로 명시하지 않아도 된다.
 	const coordsObj = {
-		latitude,
-		longitude
+		latitude,		// 	latitude: latitude,
+		longitude		// 	longitude: longitude
 	};
-	saveCords(coordsObj);
+	saveCoords(coordsObj);
+	getWeather(latitude, longitude);
 }
 function handleGeoError(position) {
 	console.log('Cant access geo location');
 }
 
-function askForCords() {
+function askForCoords() {
 	// 현재 위치를 요청한다.
 	navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
 }
 
-function loadCords() {
-	const loadedCords = localStorage.getItem(COORDS);
-	if(loadedCords === null){
-		askForCords();
+function loadCoords() {
+	const loadedCoords = localStorage.getItem(COORDS);
+	// 현재 위치가 존재하는지 여부 확인
+	if(loadedCoords === null){
+		askForCoords();
 	} else {
-		//getWeather;
+		const parsedCoords = JSON.parse(loadedCoords);
+		getWeather(parsedCoords.latitude, parsedCoords.longitude);
 	}
 }
 
 function init(){
-	loadCords();
+	loadCoords();
 }
 
 init();
